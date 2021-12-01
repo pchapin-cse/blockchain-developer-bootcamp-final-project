475a6f22a9e712dbe2cefb1811833bc33aad8e63
Philip Chapin <phil.chapin@cse-corp.com>
2021-11-30 19:30:50 -0500

    Implemented contract functions, building, testing

--- contracts/Book.sol
+++ contracts/Book.sol
@@ -1,62 +1,169 @@
 // SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.5.0 < 0.6.0;
+pragma experimental ABIEncoderV2; // allows multidimensional array returns with old solc needed for ProvableAPI
 
-pragma solidity >=0.5.0 <0.9.0;
+import "./provableAPI.sol";
 
 /**
  * @title Book
  * @dev pseudocode
  */
-contract Book {
+contract Book is usingProvable {
 
-    // function bet(string winningTeam, string losingTeam, string gameTime) public payable beforeGameTime(gameTime) {
-    //     // add a bet to the betList
-    // }
+    enum State {
+        Pending,
+        Won,
+        Lost,
+        Tied,
+        Paid,
+        Error
+    }
 
-    // function getResult(string teamName1, string teamName2, string gameTime) public payable afterGameTime(gameTime) {
-    //     // get winningTeam from Provable
-    //     // other team is losingTeam
+    struct Bet {
+        address payable maker;
+        bytes32 winningTeam;
+        bytes32 losingTeam;
+        uint gameTime;
+        uint amount;
+        State state;
+    }
 
-    //     // Collect all losing bets into winnings account.  Tally all winning bets.
-    //     // uint sumOfWinningBets = 0;
-    //     // for each bet in betList {
-    //         // if the teams and gameTime are a fuzzy match {
-    //             // if the bet is for the losing team {
-    //                 // put ether into winnings account
-    //             //} else {
-    //                 //sumOfWinningBets += bet.value;
-    //             //}
-    //         //}
-    //     //}
-    //     // uint sumOfLosingBets = winnings.value;
+    struct Query {
+        bytes32 teamName1;
+        bytes32 teamName2;
+        uint gameTime;
+        bool pending;
+    }
 
-    //     // Take profit off the top
-    //     // winnings.pay(houseAccount, sumOfWinningBets * 0.01)
-    //     // sumOfWinningBets *= 0.99;
+    Bet[] bets;
+    address payable owner;
+    mapping (bytes32 => Query) public pendingQueries;
+    
+    event LogNewProvableQuery(string description);
+    event LogNewGameResult(string winningTeam);
+
+    constructor() public payable {
+        // Set the owner to the transaction sender
+        owner = msg.sender;
+    }
+
+    function bet(bytes32 winningTeam, bytes32 losingTeam, uint gameTime) public payable beforeGameTime(gameTime) {
+        // add a bet to the betList
+        bets.push(Bet(msg.sender, winningTeam, losingTeam, gameTime, msg.value, State.Pending));
+    }
+
+    function getBets() public view returns (Bet[] memory) {
+        return bets;
+    }
+
+    function triggerPayments(bytes32 teamName1, bytes32 teamName2, uint gameTime) public afterGameTime(gameTime) {
+        emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+        // TODO?: would like to specify human readable date of UNIX gameTime for Wolfram query
+        bytes32 queryId = provable_query("WolframAlpha", strConcat("winner game result of ", bytes32ToString(teamName1), " vs ", bytes32ToString(teamName2)));
+        pendingQueries[queryId] = Query(teamName1, teamName2, gameTime, true);
+    }
+
+    
+    // Callback for getting winningTeam from Provable
+    function __callback(
+        bytes32 _myid,
+        string memory _result
+    )
+        public
+    {
+        emit LogNewGameResult(_result);
+        require(msg.sender == provable_cbAddress());
+        require (pendingQueries[_myid].pending == true);
+        bytes32 winningTeam = stringToBytes32(_result);
+        // TODO: some team names are concatted
+        emit LogNewGameResult(_result);
+        // other team is losingTeam
+        bytes32 losingTeam;
+        if (pendingQueries[_myid].teamName1 == winningTeam) {
+            losingTeam = pendingQueries[_myid].teamName2;
+        } else {
+            losingTeam = pendingQueries[_myid].teamName1;
+        }
+        uint gameTime = pendingQueries[_myid].gameTime;
+        delete pendingQueries[_myid];
+
+        // Collect all losing bets into winnings account.  Tally all winning bets.
+        uint sumOfWinningBets = 0;
+        uint sumOfLosingBets = 0;
+        //Bet[] memory winners = Bet[];
+        //Bet[] memory losers = Bet[];
+        // find all bets that match exactly
+        for (uint i = 0; i < bets.length; i++) {
+            if (bets[i].state == State.Pending && bets[i].gameTime == gameTime) {
+                if (bets[i].winningTeam == winningTeam && bets[i].losingTeam == losingTeam) {
+                    // winning bet
+                    bets[i].state = State.Won;
+                    sumOfWinningBets += bets[i].amount;
+                    //winners.push(bets[i]);
+                } else if (bets[i].winningTeam == losingTeam && bets[i].losingTeam == winningTeam) {
+                    // losing bet
+                    bets[i].state = State.Lost;
+                    sumOfLosingBets += bets[i].amount;
+                    //losers.push(bets[i]);
+                }
+                // else it's a different game still pending
+            }
+        }
         
-    //     // Pay all winning bets proportionally
-    //     // for each bet in betList {
-    //         // if the teams and gameTime are a fuzzy match {
-    //             // if the bet is for the winning team {
-    //                 // put ether into better account proportionally
-    //                 // winnings.pay(bet.account, (bet.value / sumOfWinningBets) * sumOfLosingBets);
-    //             //}
-    //         //}
-    //     //}
-    // }
+        // Take profit off the top
+        if (sumOfWinningBets != 0 || sumOfLosingBets != 0) {
+            uint ownerProfit = (sumOfWinningBets + sumOfLosingBets) / 100;
+            owner.transfer(ownerProfit);
+            sumOfWinningBets = sumOfWinningBets / 100 * 99;
+            sumOfLosingBets = sumOfLosingBets / 100 * 99;
+            // TODO: consider rounding errors a-la Superman 2?
+        }
 
-    // modifier beforeGameTime(string gameTime) {
-    //     // asserts that it is before game time
-    //     // note: need oracle?
-    // }
+        // Pay all winning bets proportionally
+        for (uint i = 0; i < bets.length; i++) {
+            if (bets[i].state == State.Won && bets[i].gameTime == gameTime && bets[i].winningTeam == winningTeam && bets[i].losingTeam == losingTeam) {
+                // winners get back 99% of (their bet plus their percentage of the losing bets)
+                bets[i].maker.transfer(sumOfWinningBets * (bets[i].amount / sumOfWinningBets) + sumOfLosingBets * (bets[i].amount / sumOfWinningBets));
+                bets[i].state = State.Paid;
+            }
+        }
+    }
 
-    // modifier afterGameTime(string gameTime) {
-    //     // asserts that it is 24 hours after game time
-    // }
+    // asserts that it is now before game time
+    modifier beforeGameTime(uint gameTime) {
+        require (now < gameTime);
+        _;
+    }
 
-    // /*
-    //     Other thoughts:
-    //     keep track of games?
-    //     save resources by not iterating through all contracts?
-    // */
+    // asserts that it is now after game time
+    modifier afterGameTime(uint gameTime) {
+        require (now > gameTime);
+        _;
+    }
+
+    // Thanks https://ethereum.stackexchange.com/questions/2519/how-to-convert-a-bytes32-to-string
+    function bytes32ToString(bytes32 _bytes32) public pure returns (string memory) {
+        uint8 i = 0;
+        while(i < 32 && _bytes32[i] != 0) {
+            i++;
+        }
+        bytes memory bytesArray = new bytes(i);
+        for (i = 0; i < 32 && _bytes32[i] != 0; i++) {
+            bytesArray[i] = _bytes32[i];
+        }
+        return string(bytesArray);
+    }
+
+    // Thanks https://ethereum.stackexchange.com/questions/9142/how-to-convert-a-string-to-bytes32
+    function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+    bytes memory tempEmptyStringTest = bytes(source);
+    if (tempEmptyStringTest.length == 0) {
+        return 0x0;
+    }
+
+    assembly {
+        result := mload(add(source, 32))
+    }
+}
 
 }
